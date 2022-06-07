84217366637ae2610471614f6cc32d2f46d30d13
jcovington16 <covingtonjoshua7@gmail.com>
2022-06-06 21:52:11 -0600

    chore: first commit, created needed components.

---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ToggleSwitch() {
+  return (
+    <div className="toggle-switch">
+        <input type="checkbox" name="name" id="id" />
+    </div>
+  )
+}
+
+export default ToggleSwitch
