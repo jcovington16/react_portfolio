d1b42b0d4dd29c25af7cb9b6a7a72be0f6f86496
jcovington16 <covingtonjoshua7@gmail.com>
2022-06-08 22:57:19 -0600

    chore: added social media icons to the page. Working on main page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Home/Profile';
+// import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      Home
+      {/* <Navbar/> */}
+      <Profile />
       
     </div>
   );
