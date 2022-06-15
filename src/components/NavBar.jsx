96576962ce250e301ddbe2a9f152a99582fe6eaa
jcovington16 <covingtonjoshua7@gmail.com>
2022-06-14 22:30:27 -0600

    chore: add navbar menu for mobile view.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -49,9 +49,18 @@ const NavBar = () => {
             {nav ? <FaTimes size={30} /> : <FaBars size={30}/>}
         </div>
 
-        <ul className="flex flex-col justify-center absolute top-0 left-0 h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-
+        {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+            {links.map(({id, link}) => (
+                <li key={id} 
+                className="px-4 cursor-pointer capitalize py-6 text-4xl">
+                    {link}
+                </li> 
+            ))}
         </ul>
+        )}
+
+
     </div>
   )
 }
